94c8bb519b41b8196be01b6343e132122890644b
pimpy6565 <155408888+pimpy6565@users.noreply.github.com>
2025-04-15 20:05:49 -0400

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
